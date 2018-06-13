0e40930e7cacc2aa4ab4d515b9253eb4f48c084e
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-06-13 16:22:47 +0200

    simple routing

--- vue-skills/src/main.js
+++ vue-skills/src/main.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import App from "./App";
+import Vue from 'vue';
 import VeeValidate from 'vee-validate';
+import App from './App';
+import router from './router';
+
 
 Vue.use(VeeValidate);
 Vue.config.productionTip = false;
@@ -14,5 +16,6 @@ Vue.config.productionTip = false;
 //   template: "<App/>"
 // });
 new Vue({
-  render: vue=>vue(App)
+  router,
+  render: vue => vue(App)
 }).$mount('#app')
